315d190afbc0fe88a4edb8036f33997ec33aad68
Rayman244 <rayhewitt42@icloud.com>
2022-01-08 15:37:37 -0500

    finished basic functionality. Need to start testing with jest

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const inquirer = require("inquirer");
-const {Manager,Intern,employeeList, Engineer} = require('./lib/brain')
-const {startQ, managerQs, engineerQs, internQs,addMoreQ } = require("./lib/Questions");
+const {Manager,Intern,employeeList, Engineer,cardList} = require('./lib/brain')
+const {startQ, managerQs, engineerQs, internQs,addMoreQs,addNewEmployeeQs } = require("./lib/Questions");
+const {generateHtml,createCard,generateCss} = require('./lib/genHtml')
+let fs = require('fs')
 
 let prompt = inquirer.createPromptModule();
 
@@ -8,43 +10,80 @@ let prompt = inquirer.createPromptModule();
 const start = () => {
 
 prompt(startQ).then((answers) =>{
-    console.log(answers);
     addManager()
 })
 }
 const addManager = () => {
 prompt(managerQs).then((answers)=>{
-    const{employeeName,employeeID,employeeEmail,officeNum} = answers
-    const managerObj = new Manager(employeeName,employeeID,employeeEmail,officeNum)
+    const{employeeName,employeeID,employeeEmail,other} = answers
+    const managerObj = new Manager(employeeName,employeeID,employeeEmail,other)
     employeeList.push(managerObj)
-    console.log(employeeList);
+    
     return addMore()
     
 })
 }
 const addEngineer = () =>{
     prompt(engineerQs).then((answers)=>{
-        const{employeeName,employeeID,employeeEmail,github} = answers
-        const engineerObj = new Engineer(employeeName,employeeID,employeeEmail,github)
+        const{employeeName,employeeID,employeeEmail,other} = answers
+        const engineerObj = new Engineer(employeeName,employeeID,employeeEmail,other)
         employeeList.push(engineerObj)
-    console.log(employeeList);
+    
 
         return addMore()
     })
 }
 const addIntern = () =>{
     prompt(internQs).then((answers)=>{
-        const{employeeName,employeeID,employeeEmail,school} = answers
-        const internObj = new Intern(employeeName,employeeID,employeeEmail,school)
+        const{employeeName,employeeID,employeeEmail,other} = answers
+        const internObj = new Intern(employeeName,employeeID,employeeEmail,other)
         employeeList.push(internObj)
-    console.log(employeeList);
+    
 
         return addMore()
     })
 }
 const addMore = () =>{
-    prompt(addMoreQ).then((answers)=>{
-        return start()
+    prompt(addMoreQs).then((answers)=>{
+        if(answers.addMoreQ === 'Yes'){
+           return addNewEmployee() 
+        }else{
+            console.log('Finished');
+            employeeList.map((emp)=>{
+                const {name,id,email,role,other} = emp
+                cardList.push(createCard(name,role,id,email,other))})
+            fs.appendFile('index.html',generateHtml(cardList), (err) =>
+            err
+              ? console.error(`Error appending to file \n ${err}`)
+              : console.log(
+                  "Successfully created HTML file."
+                )
+          )
+          fs.appendFile('styles.css',generateCss(), (err) =>
+          err
+            ? console.error(`Error appending to file \n ${err}`)
+            : console.log(
+                "Successfully created CSS file."
+              )
+        )
+            
+        }
+        
+        
+    })
+    
+}
+const addNewEmployee = ()=>{
+    prompt(addNewEmployeeQs).then((answers)=>{
+        // console.log(answers);
+        switch(answers.addNewEmployeeQ){
+            case 'Manager': addManager();
+            break;
+            case 'Engineer': addEngineer();
+            break;
+            case 'Intern': addIntern();
+            break;
+        }
         
     })
 }
